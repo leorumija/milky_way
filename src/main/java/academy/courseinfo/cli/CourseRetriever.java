cd737a667e5a301f92ff68e21e83f0489fa19068
Leonard Rumija <leorumija@yahoo.com>
2025-02-22 11:56:04 -0500

    updating project name

--- src/main/java/academy/courseinfo/cli/CourseRetriever.java
+++ src/main/java/academy/courseinfo/cli/CourseRetriever.java
@@ -1,7 +1,28 @@
 package academy.courseinfo.cli;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
-    public static void main(String[] args) {
-        System.out.println("Enter Course ID");
+    private static Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String... args) {
+        LOG.info("CourseRetriever starting");
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as first argument.");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected error", e);
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+
+        LOG.info("Retrieving courses for author '{}'", authorId);
     }
 }
